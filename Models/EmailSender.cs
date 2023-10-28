71ae98a4f9f8347133243cb57b081d351634e4ce
Aman Raj Singh <96009659+AmanrajSingh28@users.noreply.github.com>
2023-10-28 12:52:41 +0530

    Add files via upload

---
+++ Models/EmailSender.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Mail;
+
+namespace ProgrammingZone.Models
+{
+    public class EmailSender
+    {
+        public string SendTo { get; set; }
+        public string MessageBody { get; set; }
+        public string Subject { get; set; }
+        public string CC { get; set; }
+        public string AttachmentFile { get; set; }
+        public bool sendEmail()
+        {
+            SmtpClient smtp = new SmtpClient();
+            MailMessage msg = new MailMessage();
+            MailAddress from = new MailAddress("sa7496531@gmail.com");
+            MailAddress to = new MailAddress(SendTo);
+
+            //Networkcredetional Setting start here
+            NetworkCredential nc = new NetworkCredential("sa7496531@gmail.com", "khanarslaankhan2");
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.Credentials = nc;
+            //Networkcredetional Setting close here
+
+            //MailMessage Setting start here
+            msg.Sender = from;
+            msg.Subject = Subject;
+            msg.To.Add(to);
+            msg.From = from;
+            msg.Body = MessageBody;
+            if (CC != null)
+            {
+                Attachment att = new Attachment(HttpContext.Current.Server.MapPath(AttachmentFile));
+                msg.Attachments.Add(att);
+            }
+            smtp.Send(msg);
+            return true;
+        }
+    }
+}
---
+++ Models/SMSSender.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace ProgrammingZone.Models
+{
+    public class SMSSender
+    {
+        string user, password, sender, priorty, stype;
+        public SMSSender()
+        {
+            user = "MTECHL";
+            password = "erdirector";
+            sender = "MTECHL";
+            priorty = "ndnd";
+            stype = "normal";
+        }
+        public bool SendSms(string mobile, string msg)
+        {
+            string url = "http://trans.smsfresh.co/api/sendmsg.php?user=" + user + "&pass=" + password + "&sender=" + sender + "&phone=" + mobile + "&text=" + msg + "&priority=" + priorty + "&stype=" + stype + "";
+            WebClient client = new WebClient();
+            string s = client.DownloadString(url);
+            char ch = s[0];
+            if (ch == 'S')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
